3372140fb33c0499639c23d1fc3c58461e0e8e42
Yuliia Bohosian <yuliia.bohosian@gmail.com>
2023-02-25 18:35:25 +0000

    SimpleSpan method

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -25,6 +25,10 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
+
+#processor = SimpleSpanProcessor(ConsoleSpanExporter())
+#provider.add_span_processor(simple_processor)
+
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
