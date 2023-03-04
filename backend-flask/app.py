f9a99d72bfc7cb78cce4b5ff776fcf05b1d5a967
Yuliia Bohosian <yuliia.bohosian@gmail.com>
2023-03-04 04:22:39 +0000

    setting up CloudWatch Logs

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -3,6 +3,7 @@ from flask import request
 from flask_cors import CORS, cross_origin
 import os
 
+
 from services.notifications_activities import *
 from services.home_activities import *
 from services.user_activities import *
@@ -23,6 +24,20 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 #XRay......
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
+#Clouwatch logs
+import watchtower
+import logging
+from time import strftime
+
+# Configuring Logger to Use CloudWatch
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+LOGGER.addHandler(console_handler)
+LOGGER.addHandler(cw_handler)
+LOGGER.info('Hello Cloudwatch! from  /api/activities/home')
 
 
 # Initialize tracing and an exporter that can send data to Honeycomb
@@ -58,6 +73,12 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
+
+@app.after_request
+def after_request(response):
+    timestamp = strftime('[%Y-%b-%d %H:%M]')
+    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+    return response
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,6 +1,10 @@
 from datetime import datetime, timedelta, timezone
+from opentelemetry import trace
+import logging
+
 class HomeActivities:
   def run():
+    LOGGER.info('Hello Cloudwatch! from  /api/activities/home')
     now = datetime.now(timezone.utc).astimezone()
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
