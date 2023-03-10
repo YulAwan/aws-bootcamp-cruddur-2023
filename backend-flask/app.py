3f65cc8c566896dd3cd5d6cc118d31714f4e2bf8
Yuliia Bohosian <yuliia.bohosian@gmail.com>
2023-03-10 19:03:18 +0000

    JWT Server side

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from flask_cors import CORS, cross_origin
 import os
+import sys
 
 
 from services.notifications_activities import *
@@ -78,8 +79,8 @@ origins = [frontend, backend]
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
-  expose_headers="location,link",
-  allow_headers="content-type,if-modified-since",
+  headers=['Content-Type', 'Authorization'], 
+  expose_headers='Authorization',
   methods="OPTIONS,GET,HEAD,POST"
 )
 
