8db273c404d693bf54332eb05d589f2b8d03ffb9
Yuliia Bohosian <yuliia.bohosian@gmail.com>
2023-03-11 17:35:26 +0000

    setting backend

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -15,6 +15,10 @@ from services.message_groups import *
 from services.messages import *
 from services.create_message import *
 from services.show_activity import *
+
+from lib.cognito_jwt_token import CognitoJwtToken, extract_access_token, TokenVerifyError
+
+
 #Honeycomb
 from opentelemetry import trace
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
@@ -67,9 +71,20 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
+
+cognito_jwt_token = CognitoJwtToken(
+  user_pool_id=os.getenv("AWS_COGNITO_USER_POOL_ID"), 
+  user_pool_client_id=os.getenv("AWS_COGNITO_USER_POOL_CLIENT_ID"),
+  region=os.getenv("AWS_DEFAULT_REGION")
+)
+#app.config ['AWS_COGNITO_USER_POOL_ID'] = os.getenv ('AWS_COGNITO_USER_POOL_ID')
+#app.config ['AWS_COGNITO_USER_POOL_CLIENT_ID'] = os.getenv ('AWS_COGNITO_USER_POOL_CLIENT_ID')
+#aws_auth = AWSCognitoAuthentication (app)
+
 #Xray
 #XRayMiddleware(app, xray_recorder)
 
+# Initialize automatic instrumentation with Flask
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
 
@@ -83,7 +98,7 @@ cors = CORS(
   expose_headers='Authorization',
   methods="OPTIONS,GET,HEAD,POST"
 )
-
+#CloudWatch Logs
 #@app.after_request
 #def after_request(response):
 #    timestamp = strftime('[%Y-%b-%d %H:%M]')
@@ -150,8 +165,21 @@ def data_create_message():
   return
 
 @app.route("/api/activities/home", methods=['GET'])
+#@xray_recorder.capture('activities_home')
+
 def data_home():
-  data = HomeActivities.run()
+  access_token = extract_access_token(request.headers)
+  try:
+    claims = cognito_jwt_token.verify(access_token)
+    # authenicatied request
+    app.logger.debug("authenicated")
+    app.logger.debug(claims)
+    app.logger.debug(claims['username'])
+    data = HomeActivities.run(cognito_user_id=claims['username'])
+  except TokenVerifyError as e:
+  #unauthenticated request
+    app.logger.debug(e)
+    app.logger.debug('unauthenticated')
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
