471413b004fd714ec189225ab8882115c63a2811
Yuliia Bohosian <yuliia.bohosian@gmail.com>
2023-04-02 03:41:38 +0000

    fixed the crud posting activity

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -83,39 +83,39 @@ class Db:
         else:
           return json[0]
 
-def query_wrap_object(self,template):
-  sql = f"""
-  (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
-  {template}
-  ) object_row);
-  """
-  return sql
+  def query_wrap_object(self,template):
+    sql = f"""
+    (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
+    {template}
+    ) object_row);
+    """
+    return sql
 
-def query_wrap_array(self,template):
-  sql = f"""
-  (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
-  {template}
-  ) array_row);
-  """
-  return sql
+  def query_wrap_array(self,template):
+    sql = f"""
+    (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
+    {template}
+    ) array_row);
+    """
+    return sql
 
-def print_sql_err(self,err):
-# get details about the exception
-  err_type, err_obj, traceback = sys.exc_info()
+  def print_sql_err(self,err):
+  # get details about the exception
+    err_type, err_obj, traceback = sys.exc_info()
 
-# get the line number when exception occured
-  line_num = traceback.tb_lineno
+  # get the line number when exception occured
+    line_num = traceback.tb_lineno
 
-# print the connect() error
-  print ("\npsycopg ERROR:", err, "on line number:", line_num)
-  print ("psycopg traceback:", traceback, "-- type:", err_type)
+  # print the connect() error
+    print ("\npsycopg ERROR:", err, "on line number:", line_num)
+    print ("psycopg traceback:", traceback, "-- type:", err_type)
 
-# print the pgcode and pgerror exceptions
-  print ("pgerror:", err.pgerror)
-  print ("pgcode:", err.pgcode, "\n")
+  # print the pgcode and pgerror exceptions
+    print ("pgerror:", err.pgerror)
+    print ("pgcode:", err.pgcode, "\n")
 
-#connection_url = os.getenv("CONNECTION_URL")
-#pool = ConnectionPool(connection_url)
+  #connection_url = os.getenv("CONNECTION_URL")
+  #pool = ConnectionPool(connection_url)
 db = Db()
 
 #connection_url = os.getenv("CONNECTION_URL")
